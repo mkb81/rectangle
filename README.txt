00973e8779bd3c0dead2189ef6235643fd30584a
Marc Kevin Bendig <mkbendig@gmail.com>
2018-04-01 18:09:37 +0200

    Init-Version

---
+++ README.txt
@@ -0,0 +1,6 @@
+This is a Pygame (1.9.3) exercise with Python 3.6.4
+
+Use the cursor keys to control a rectangle and change the color with the space bar.  pPress the'q' key to exit.
+
+Start
+python3 start_test.py
---
+++ start_test.py
@@ -0,0 +1,19 @@
+import sys
+from rechteck import LittleGame
+
+
+def main():
+    """
+    Start Pygane example
+
+    :return: -
+    :rtype: -
+    """
+    spped = 5
+    my_game = LittleGame(spped)
+    my_game.start_game()
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
